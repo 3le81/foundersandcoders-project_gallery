8fb7816ef4e54c5750fd7da6255ccf99a08cbb0c
Elena <57794968+3le81@users.noreply.github.com>
2023-05-26 14:32:27 +0100

    two more cards added, the hobby page and the project game,but not completed yet;javascript just started as a test and some changes to CSS too

---
+++ jscript.js
@@ -0,0 +1,21 @@
+function documt() {
+    document.getElementById("document").innerHTML = "This page was created for showing my Document Page on CSS";
+}
+
+function landing() {
+    document.getElementById("landing").innerHTML = "This page was created for showing my historic page about Julius Caesar";
+}
+
+function historic() {
+    document.getElementById("historic").innerHTML = "This page was created for showing my historic page about Julius Caesar";
+}
+function portfolio() {
+    document.getElementById("portfolio").innerHTML = "This page was created for showing my portfolio so far";
+}
+function hobby() {
+    document.getElementById("hobby").innerHTML = "This page is a project for Founders and Coders";
+}
+function game() {
+    document.getElementById("game").innerHTML = "This page is a project for toddlers game";
+}
+
