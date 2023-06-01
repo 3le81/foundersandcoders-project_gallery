4c09278c6d7a98ce3903164ee4ff9619704328bc
Elena <57794968+3le81@users.noreply.github.com>
2023-06-01 14:09:17 +0100

    more cards of project added and png of the projects included in the page, not completed yet

--- jscript.js
+++ jscript.js
@@ -9,13 +9,21 @@ function landing() {
 function historic() {
     document.getElementById("historic").innerHTML = "This page was created for showing my historic page about Julius Caesar";
 }
-function portfolio() {
-    document.getElementById("portfolio").innerHTML = "This page was created for showing my portfolio so far";
+function restaurant() {
+    document.getElementById("restaurant").innerHTML = "This page was created for showing my project for an italian restaurant";
 }
 function hobby() {
-    document.getElementById("hobby").innerHTML = "This page is a project for Founders and Coders";
+    document.getElementById("hobby").innerHTML = "This Hobby page is one of my projects for Founders and Coders";
 }
 function game() {
-    document.getElementById("game").innerHTML = "This page is a project for toddlers game";
+    document.getElementById("game").innerHTML = "This page is my project for a toddlers game";
 }
+
+const button = document.getElementById('btn_about');
+const div = document.getElementById('about');
+
+function about() {
+    div.innerHTML = '<p>I started coding a while ago, while I was pregnant with my second baby, and I just fell in love with this world..and I keep learning new and exciting things every day now!</p>';
+}
+button.addEventListener('click', about);
 
